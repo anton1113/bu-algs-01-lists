36f4d6787a4a0bab0f905da074a77178b04a0762
Anton Rasshchektaiev <anton.rasshchektaiev@auto1.com>
2024-03-25 13:31:56 +0100

    add comparable example

--- src/main/java/com/bu/algs/compare/Human.java
+++ src/main/java/com/bu/algs/compare/Human.java
@@ -1,2 +1,58 @@
-package com.bu.algs.compare;public class Student {
+package com.bu.algs.compare;
+
+public class Human implements Comparable<Human> {
+
+    private double height;
+    private double weight;
+    private int age;
+
+    public Human(double height, double weight, int age) {
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Human o) {
+        if (age > o.age) {
+            return 1;
+        } else if (age == o.age) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "height=" + height +
+                ", weight=" + weight +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/com/bu/algs/compare/HumanCompareApp.java
+++ src/main/java/com/bu/algs/compare/HumanCompareApp.java
@@ -1,2 +1,25 @@
-package com.bu.algs.compare;public class HumanCompareApp {
+package com.bu.algs.compare;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HumanCompareApp {
+
+    public static void main(String[] args) {
+
+        Human h1 = new Human(150.0, 63.0, 16);
+        Human h2 = new Human(150.0, 63.0, 6);
+        Human h3 = new Human(195.0, 90.0, 26);
+
+        List<Human> lh = new ArrayList<>();
+        lh.add(h1);
+        lh.add(h2);
+        lh.add(h3);
+
+        int older = h1.compareTo(h2);
+
+        Collections.sort(lh);
+        System.out.println(lh);
+    }
 }
